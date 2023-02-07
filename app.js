7bf0709506a9219bb29e0e63e2c64e2702591309
GrandDataMaster <99348571+KPROsoft21@users.noreply.github.com>
2023-02-08 02:21:33 +0300

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ right.addEventListener('click', function () {
 
 function getImage() {
     let zero = '0';
-    let number = Math.ceil((Math.random() * 104));
+    let number = Math.ceil((Math.random() * 107));
     console.log(number)
     if (number != 10) { // creating the file number by adding zeroW
         number = number;
