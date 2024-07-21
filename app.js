f3fbc0b3b7531dd2ceeb4b672d3eb827488edec1
GrandDataMaster <99348571+KPROsoft21@users.noreply.github.com>
2024-07-21 10:30:16 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,29 +1,34 @@
-let left = document.getElementById('left');
-let right = document.getElementById('right');
-
-var leftNum;
-var rightNum;
+document.addEventListener('DOMContentLoaded', () => {
+    leftNum = getImage();
+    rightNum = getImage();
+    
+    // Ensure leftNum and rightNum are not the same
+    while (rightNum === leftNum) {
+        rightNum = getImage();
+    }
+    
+    left.setAttribute('src', `img/${leftNum}.png`);
+    right.setAttribute('src', `img/${rightNum}.png`);
+});
 
 left.addEventListener('click', function () {
-    rightNum = getImage();
-    right.setAttribute('src', `img/${rightNum}.png`)
+    let newRightNum;
+    do {
+        newRightNum = getImage();
+    } while (newRightNum === leftNum);
+    rightNum = newRightNum;
+    right.setAttribute('src', `img/${rightNum}.png`);
 });
+
 right.addEventListener('click', function () {
-    leftNum = getImage();
-    left.setAttribute('src', `img/${leftNum}.png`)
+    let newLeftNum;
+    do {
+        newLeftNum = getImage();
+    } while (newLeftNum === rightNum);
+    leftNum = newLeftNum;
+    left.setAttribute('src', `img/${leftNum}.png`);
 });
 
 function getImage() {
-    let zero = '0';
-    let number = Math.ceil((Math.random() * 783));
-    console.log(number)
-    if (number != 10) { // creating the file number by adding zeroW
-        number = number;
-    }
-
-    if (number != leftNum && number != rightNum) {
-        return number;
-    } else {
-        return getImage();
-    }
+    return Math.ceil(Math.random() * 783);
 }
